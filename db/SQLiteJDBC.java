95855a028e817464b9fe1e37435f6487145bad34
root <root@raspberrypi.(none)>
2015-04-23 08:54:31 +0000

    Adds database files

---
+++ db/SQLiteJDBC.java
@@ -0,0 +1,15 @@
+public class SQLiteJDBC
+{
+  public static void main( String args[] )
+  {
+    Connection c = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:/var/www/db/database.db");
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Opened database successfully");
+  }
+}
